53ac80f5a13505415ff9ab2e83c87b212350fbc5
TheNoodleMoose <cjh605@gmail.com>
2019-01-28 14:26:24 -0500

    Adding production build files

---
+++ client/build/precache-manifest.75bdc5d437504ae78071711c40613cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8eb4f49e07e2ef954f6b",
+    "url": "/static/js/main.8eb4f49e.chunk.js"
+  },
+  {
+    "revision": "48537cedb70842155111",
+    "url": "/static/js/1.48537ced.chunk.js"
+  },
+  {
+    "revision": "8eb4f49e07e2ef954f6b",
+    "url": "/static/css/main.fd402ad5.chunk.css"
+  },
+  {
+    "revision": "77e75200da74a3a2a51ebd4cd2623c93",
+    "url": "/index.html"
+  }
+];
