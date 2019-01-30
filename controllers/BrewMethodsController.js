345180276a3a0e40871dff667c065b22693ae668
Austin Sharman <asharman22@gmail.com>
2019-01-30 08:53:15 -0500

    Added brew methods and steps to database

---
+++ controllers/BrewMethodsController.js
@@ -0,0 +1,39 @@
+const db = require('../models');
+
+module.exports = {
+  async addBrewMethod(req, res) {
+    const { name, defaultGrindSize } = req.body;
+    const brewMethod = await db.BrewMethod.create({
+      name,
+      defaultGrindSize
+    });
+
+    res.json(brewMethod);
+  },
+
+  async addBrewStep(req, res) {
+    const { BrewingMethod, StepTitle, SvgPath, SubText } = req.body;
+    const brewStep = await db.BrewStep.create({
+      BrewingMethod,
+      StepTitle,
+      SvgPath,
+      SubText
+    });
+
+    res.json(brewStep);
+  },
+
+  async searchBrewSteps(req, res) {
+    const { BrewMethod } = req.body;
+    const brewSteps = await db.BrewMethod.findOne({
+      where: {
+        name: BrewMethod
+      },
+      include: [{
+        model: db.BrewStep
+      }]
+    });
+
+    res.json(brewSteps);
+  }
+}
---
+++ models/BrewSteps.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+  var BrewStep = sequelize.define("BrewStep", {
+    // The email cannot be null, and must be a proper email before creation
+    BrewingMethod: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    StepTitle: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    SvgPath: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    SubText: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return BrewStep;
+};
