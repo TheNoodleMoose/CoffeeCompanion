5273b94e01835a1fa133e1ae6ae52250efbb2348
Austin Sharman <asharman22@gmail.com>
2019-03-26 12:58:22 -0400

    Added flex wrappers and containers for carousel

--- client/src/Components/SubComponents/CardCarousel.js
+++ client/src/Components/SubComponents/CardCarousel.js
@@ -2,17 +2,36 @@ import React from 'react';
 import styled, { keyframes, css } from 'styled-components';
 
 const CardCarousel = ({ brewSteps, stage, blinking }) => (
-  <Card blinking={blinking}>
-    <h3>{brewSteps[stage].StepTitle}</h3>
-    <img
-      src={require(`../../assets/images/${brewSteps[stage].SvgPath}.svg`)}
-      alt={brewSteps[stage].SvgPath}
-    />
-    <SubText>{brewSteps[stage].SubText}</SubText>
-  </Card>
+  <Wrapper>
+    <Carousel>
+      {brewSteps.map(step => (
+        <Card blinking={blinking}>
+          <h3>{step.StepTitle}</h3>
+          <img
+            src={require(`../../assets/images/${step.SvgPath}.svg`)}
+            alt={step.SvgPath}
+          />
+          <SubText>{step.SubText}</SubText>
+        </Card>
+      ))}
+    </Carousel>
+  </Wrapper>
 );
 
 export default CardCarousel;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Carousel = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  width: 380px;
+  overflow: hidden;
+`;
 
 const blink = keyframes`
   50% {
@@ -28,10 +47,9 @@ const Card = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin: 0 auto;
-  margin-top: 24px;
+  margin: 24px 30px;
   padding: 10px;
-  width: 300px;
+  min-width: 300px;
   height: auto;
   background-color: #f3f1ee;
   color: #2f2923;
